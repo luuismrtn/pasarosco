aafe21795e9c10a941369f8d340ff3c3bb964d63
luism <unconfigured@null.spigotmc.org>
2024-12-01 18:27:22 +0100

    Responsive para desktop hecho

--- src/components/ButtonSection.tsx
+++ src/components/ButtonSection.tsx
@@ -49,7 +49,7 @@ const ButtonSection = ({
     small: "px-6 py-2 text-lg lg:text-sm lg:px-4 lg:py-2 xl:text-base xl:px-6 xl:py-2",
     icon: "px-4 py-2 text-xl lg:text-base lg:px-3 lg:py-1 xl:text-lg xl:px-4 xl:py-1",
     medium: "px-9 py-3 text-lg",
-    large: "px-8 py-4 text-3xl sm:text-xl lg:text-2xl lg:px-6 lg:py-3 xl:text-3xl xl:px-6 xl:py-3",
+    large: "px-8 py-4 text-3xl lg:text-2xl lg:px-4 lg:py-3 xl:text-3xl xl:px-6 xl:py-3",
   };
 
   const handleClick = () => {
